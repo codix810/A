77951f9eda154628cac49fb021e54b971c7f0f22
CodiX 🧑‍💻 <codix810@gmail.com>
2026-03-01 01:46:54 +0200

    Refactor DELETE and PUT functions to use context

--- src/app/api/admin/products/[id]/route.ts
+++ src/app/api/admin/products/[id]/route.ts
@@ -2,26 +2,33 @@ import { NextResponse } from "next/server";
 import connectDB from "../../../../../lib/db";
 import Product from "../../../../../models/Product";
 
+/* DELETE PRODUCT */
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  context: any
 ) {
   await connectDB();
-  await Product.findByIdAndDelete(params.id);
+
+  const id = context.params.id;
+
+  await Product.findByIdAndDelete(id);
 
   return NextResponse.json({ message: "Deleted" });
 }
 
+/* UPDATE PRODUCT */
 export async function PUT(
   req: Request,
-  { params }: { params: { id: string } }
+  context: any
 ) {
   const body = await req.json();
 
   await connectDB();
 
+  const id = context.params.id;
+
   const product = await Product.findByIdAndUpdate(
-    params.id,
+    id,
     body,
     { new: true }
   );
