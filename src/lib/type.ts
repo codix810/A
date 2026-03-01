36dbacba56dc16ad7b7d4d0c7bc0580ae2be0251
CodiX 🧑‍💻 <codix810@gmail.com>
2026-03-01 03:26:38 +0200

    Update type.ts

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,8 +1,11 @@
-export interface typeProduct{
-    id: number;
-    title: string;
-    description: string;
-    price: number;
-    image: string;
-    quantity?: number;
+
+
+export interface typeProduct {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+  hasOffer?: number;
+  quantity?: number;
 }
