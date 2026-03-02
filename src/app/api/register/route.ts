dc869f2ec05496cf0d9156a1e4f38236b5731e12
CodiX 🧑‍💻 <codix810@gmail.com>
2026-03-03 00:25:43 +0200

    Update route.ts

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,34 +1,75 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import { jwtVerify } from "jose";
-
-export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("token")?.value;
-
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+import connectDB from "../../../lib/db";
+import UserA from "../../../models/userA";
+export async function POST(req: Request) {
   try {
-    const secret = new TextEncoder().encode(
-      process.env.JWT_SECRET
-    );
+    const { name, email, password } = await req.json();
 
-    const { payload } = await jwtVerify(token, secret);
-
-    // حماية الادمن
-    if (req.nextUrl.pathname.startsWith("/admin")) {
-      if (payload.role !== "admin") {
-        return NextResponse.redirect(new URL("/", req.url));
-      }
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { error: "All fields required" },
+        { status: 400 }
+      );
     }
 
-    return NextResponse.next();
-  } catch {
-    return NextResponse.redirect(new URL("/login", req.url));
+    await connectDB();
+
+    const existingUser = await UserA.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Email already exists" },
+        { status: 400 }
+      );
+    }
+
+    const usersCount = await UserA.countDocuments();
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const role = usersCount === 0 ? "admin" : "user";
+
+    const newUser = await UserA.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    // ✅ إنشاء التوكن
+    const token = jwt.sign(
+      {
+        userId: newUser._id,
+        email: newUser.email,
+        role: newUser.role,
+      },
+      process.env.JWT_SECRET!,
+      { expiresIn: "7d" }
+    );
+
+    // ✅ تخزين الكوكي بشكل صحيح للـ Production
+    const cookieStore = await cookies();
+
+    cookieStore.set("token", token, {
+      httpOnly: true,
+      path: "/",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+    });
+
+    return NextResponse.json({
+      message: "Account created successfully",
+    });
+
+  } catch (error: any) {
+    console.log("REGISTER ERROR:", error);
+
+    return NextResponse.json(
+      { error: "Server error" },
+      { status: 500 }
+    );
   }
 }
-
-export const config = {
-  matcher: ["/admin/:path*"],
-};
